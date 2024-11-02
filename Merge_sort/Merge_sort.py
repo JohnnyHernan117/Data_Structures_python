3eb8987622c0337f15422653ddb3c60f891d1b75
Johnny Hernandez <johnnyhernan117@gmail.com>
2024-11-01 20:01:55 -0700

    Add files via upload

---
+++ Merge_sort/Merge_sort.py
@@ -0,0 +1,60 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] < right_arr[j]
+            j += 1
+            k += 1
+
+def quicksort(arr, left, right):
+    if left < right:
+        partition_pos = partition(arr, left, right)
+        quicksort(arr, left, partition_pos - 1)
+        quicksort(arr, partition_pos + 1, right)
+ 
+def partition(arr, left, right):
+    i = left
+    j = right - 1
+    pivot = arr[right]
+
+    while i < j:
+        while i < right and arr[i] < pivot:
+            i += 1
+
+        while j > left and arr[j] >= pivot:
+            j -= 1
+
+        if i < j:
+            arr[i], arr[j] = arr[j],arr[i]
+
+    if arr[i] > pivot:
+        arr[i], arr[right] = arr[right], arr[i]
+
+    return i
+
+arr1 = [14, 46, 43, 27, 57, 41, 45, 21, 70]
+quicksort(arr1)
+print(arr1)
